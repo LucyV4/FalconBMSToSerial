cb998773ea6f4e01588ac4f9615336433b1f00f5
LucyV4 <lars.verkerk.pc@gmail.com>
2025-08-03 16:39:01 +0200

    Testing

--- send_serial_data.py
+++ send_serial_data.py
@@ -1,10 +1,25 @@
-import os, time
+import os, time, sys
+import importlib.util
 import serial
+
+# Full absolute path to your file
+file_path = os.path.abspath("./falcon-memreader/falcon-memreader.py")
+
+# Give it a valid Python module name (replace dash with underscore for convenience)
+module_name = "falcon_memreader"
+
+spec = importlib.util.spec_from_file_location(module_name, file_path)
+module = importlib.util.module_from_spec(spec)
+sys.modules[module_name] = module
+spec.loader.exec_module(module)
+
+module.examples()
 
 serial_port = serial.Serial("/dev/ttyACM0", 115200)
 
 test_package = bytes(3184)
 test_package = os.urandom(3184)
+
 
 while True:
 	serial_port.write(test_package)
