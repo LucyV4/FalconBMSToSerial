1ef71b2a7ed81a9727a411e34580e45d643dad9c
LucyV4 <lucy@pipeframe.xyz>
2025-08-04 14:34:16 +0200

    Update imports

--- memreader.py
+++ memreader.py
@@ -2,17 +2,12 @@ import os, sys
 import importlib.util
 import ctypes
 
-file_path = os.path.abspath("./falcon_memreader/falcon-memreader.py")
-module_name = "falcon_memreader"
-spec = importlib.util.spec_from_file_location(module_name, file_path)
-module = importlib.util.module_from_spec(spec)
-sys.modules[module_name] = module
-spec.loader.exec_module(module)
+from falcon_memreader import falcon_memreader
 
 class BMSMemReader:
 	def create_package(self) -> bytes:
-		flightdata: ctypes.Structure = module.read_shared_memory(module.FlightData)
-		flightdata2: ctypes.Structure = module.read_shared_memory(module.FlightData2)
+		flightdata: ctypes.Structure = falcon_memreader.read_shared_memory(falcon_memreader.FlightData)
+		flightdata2: ctypes.Structure = falcon_memreader.read_shared_memory(falcon_memreader.FlightData2)
 		
 		flightdata_bytes = ctypes.string_at(ctypes.byref(flightdata), ctypes.sizeof(flightdata))
 		flightdata2_bytes = ctypes.string_at(ctypes.byref(flightdata2), ctypes.sizeof(flightdata2))
