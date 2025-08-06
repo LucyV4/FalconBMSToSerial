0415b5e8eef737ac4eb852bcec0071f3fc875e13
LucyV4 <lucy@pipeframe.xyz>
2025-08-06 02:00:13 +0200

    Found unhandled exception

--- memreader.py
+++ memreader.py
@@ -1,8 +1,12 @@
 import ctypes
 from falcon_memreader import falcon_memreader
+from logger import log_error
 
 def check_game_active() -> bool:
 	data = falcon_memreader.read_shared_memory_stringdata()
+	if data == None:
+		log_error("Problem when accessing game memory")
+		return False
 	return str(data.BmsExe) != ""
 
 def get_game_version() -> str:
@@ -12,9 +16,9 @@ def get_game_version() -> str:
 def create_package() -> bytes:
 	flightdata: ctypes.Structure = falcon_memreader.read_shared_memory(falcon_memreader.FlightData)
 	flightdata2: ctypes.Structure = falcon_memreader.read_shared_memory(falcon_memreader.FlightData2)
-		
+	
 	flightdata_bytes = ctypes.string_at(ctypes.byref(flightdata), ctypes.sizeof(flightdata))
 	flightdata2_bytes = ctypes.string_at(ctypes.byref(flightdata2), ctypes.sizeof(flightdata2))
-		
+
 	data = flightdata_bytes + flightdata2_bytes
 	return data
